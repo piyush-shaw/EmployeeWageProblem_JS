81459a3fa82790ce107058044bf83bd8c8f8fc6a
piyush-shaw <piyushshaw0000@gmail.com>
2023-03-09 17:39:59 +0530

    [Piyush] added to refactor code to write function for daily working hrs

--- EmployeeWage/employee.js
+++ EmployeeWage/employee.js
@@ -1,4 +1,4 @@
-//UC 2 - To calculate employee daily wage 
+//UC 3 - Refactor code to function to get work hrs 
 const IS_Absent=0;
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
@@ -6,21 +6,23 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8
 const WAGE_PER_HOURS=20;
 
-let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck)
+function getWorkingHours(empCheck)
 {
-  case IS_PART_TIME:
-     empHrs=PART_TIME_HOURS;
-     break;        
-        
-  case IS_FULL_TIME:
-     empHrs=FULL_TIME_HOURS;
-     break;    
-     default:
-         empHrs=0        
+    switch(empCheck)
+    {
+      case IS_PART_TIME:
+          return PART_TIME_HOURS;        
+            
+      case IS_FULL_TIME:
+          return FULL_TIME_HOURS;  
+
+          default:
+             return 0;       
+    }
 }
 
-
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10)%3;
+empHrs= getWorkingHours(empCheck);
 let empWage=empHrs*WAGE_PER_HOURS;
 console.log("emp Wage :"+empWage);
